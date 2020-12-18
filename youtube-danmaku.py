bd5c7cb675411be841f52ecda5fcdfb0c313463c
hare1039 <hare39393@gmail.com>
2020-12-18 15:52:45 -0500

    Change filename output

--- youtube-danmaku.py
+++ youtube-danmaku.py
@@ -12,11 +12,13 @@ CHAT_DL_PATH="/Users/hare1039/Documents/gitprojects/chat-replay-downloader"
 
 def ytdl(url):
     ydl_opts = {
-        "outtmpl": "%(id)s.%(ext)s",
+        "outtmpl": "%(title)s-%(id)s.%(ext)s",
         "merge_output_format": "mkv"
     }
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        return ydl.extract_info(url)
+        result = ydl.extract_info(url)
+        return result, ydl.prepare_filename(result)
 
 def ytdl_comments(url, jsonname):
     r = subprocess.run(["python3",
@@ -27,7 +29,7 @@ def ytdl_comments(url, jsonname):
                         url])
     return r.returncode == 0
 
-def convert_yt_comments(jsonname, comment_duration, outputname):
+def convert_yt_comments(jsonname, comment_duration, video_info, outputname):
     with open(jsonname) as f:
         yt_comments = json.load(f)
 
@@ -47,6 +49,9 @@ def convert_yt_comments(jsonname, comment_duration, outputname):
 
     for msg in yt_comments:
         now = msg["video_offset_time_msec"]
+        if now > video_info["duration"] * 1000:
+#            print(now, ">", video_info["duration"] * 1000)
+            continue
 
         if not msg["message"]:
             continue
@@ -73,27 +78,41 @@ def convert_yt_comments(jsonname, comment_duration, outputname):
 
 def main(args):
     for url in args.urls:
-        result = ytdl(url)
+        result, vid_filename = ytdl(url)
         id = result["id"]
+        os.rename(vid_filename, id + ".mkv")
 
+        print("[danmaku] Downloading live comments")
         ok = ytdl_comments(url, id + ".json")
         if not ok:
-            print("cannot download live comments")
+            print("[danmaku] cannot download live comments")
             continue
 
-        convert_yt_comments(id + ".json", args.duration, id + ".ass")
+        print("[danmaku] converting to .ass")
+        convert_yt_comments(id + ".json", args.duration, result, id + ".ass")
 
-        subprocess.run(["ffmpeg",
-                        "-i", id + ".mkv",
-                        "-i", id + ".ass",
-                        "-c:a", "copy",
-                        "-c:v", "copy",
-                        "-map", "0",
-                        "-map", "1",
-                        "-hide_banner",
-                        "-loglevel", "panic",
-                        "-y",
-                        result["title"] + "-" + id + ".mkv"])
+        print("[danmaku] adding .ass to mkv")
+        try:
+            subprocess.run(["mkvmerge",
+                            "--output", vid_filename,
+                            id + ".mkv",
+                            "--track-name", "0:youtube-comments",
+                            id + ".ass"])
+
+        except FileNotFoundError:
+            print("[danmaku] mkvmerge not found. Use ffmpeg.")
+
+            subprocess.run(["ffmpeg",
+                            "-i", id + ".mkv",
+                            "-i", id + ".ass",
+                            "-c:a", "copy",
+                            "-c:v", "copy",
+                            "-map", "0",
+                            "-map", "1",
+                            "-hide_banner",
+                            "-loglevel", "panic",
+                            "-y",
+                            vid_filename])
 
         os.remove(id + ".mkv")
         os.remove(id + ".json")
@@ -101,7 +120,6 @@ def main(args):
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--start", type=int, help="start")
     parser.add_argument("--duration", type=int, default=5000, help="comment display duration")
     parser.add_argument("urls", nargs='*', type=str)
     return parser
